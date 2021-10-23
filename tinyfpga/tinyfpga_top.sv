19c5c1a5acbfc5eb7825e451f37e7116054e6652
Omar Rizwan <omar@omar.website>
2021-10-23 09:06:53 -0700

    Checkpoint: output most_recent_kbd_data on logic analyzer lines.
    (Doesn't work; pins just go low. I think it's just that I need to fix the rst pin.)

--- tinyfpga/tinyfpga_top.sv
+++ tinyfpga/tinyfpga_top.sv
@@ -5,13 +5,24 @@ module tinyfpga_top (
     input wire   logic CLK, // 16MHz clock
 
     inout logic  PIN_10,
-    inout logic  PIN_11
+    inout logic  PIN_11,
+
+    // logic analyzer pins 0 - 3
+    output logic PIN_1,
+    output logic PIN_2,
+    output logic PIN_3,
+    output logic PIN_4
   );
+
+    logic [7:0]  most_recent_kbd_data;
 
     top top_inst (
       .clk(CLK), .rst('1),
       .kbd_clk(PIN_11),
-      .kbd_data(PIN_10)
+      .kbd_data(PIN_10),
+      .most_recent_kbd_data
     );
+
+    assign {PIN_1, PIN_2, PIN_3, PIN_4} = most_recent_kbd_data[0:3];
 
 endmodule // tinyfpga_top
