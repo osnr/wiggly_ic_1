28d9696cdb52926af8f6e6eb00537c2fd6f4fe9d
Omar Rizwan <omar@omar.website>
2021-11-12 10:53:36 +0100

    Add mouse tester

--- test/test_wiggly_ic_1.py
+++ test/test_wiggly_ic_1.py
@@ -6,6 +6,64 @@ from cocotb.triggers import FallingEdge, RisingEdge, Timer
 import png
 from datetime import datetime
 
+codes_to_send = []
+async def mouse(dut):
+    while True:
+        while len(codes_to_send) == 0:
+            dut.mouse_clk_in = 1
+            dut.mouse_data_in = 1
+            await Timer(1, units="us")
+
+        code = codes_to_send.pop(0)
+        
+        start = 0
+        parity = 0
+        stop = 1
+
+        num_ones = 0
+
+        bits = []
+        bits.append(start)
+        for i in range(0, 8):
+            bit = (code >> i) & 0x01
+            bits.append(bit)
+            num_ones += bit
+        parity = num_ones % 2 == 0
+        bits.append(parity)
+        bits.append(stop)
+
+        dut.mouse_clk_in = 1
+        dut.mouse_data_in = 1
+        await Timer(50, units="us")
+
+        i = 0
+        for bit in bits:
+            dut.mouse_clk_in = 1
+            await Timer(20, units="us")
+            dut.mouse_data_in = bit
+            await Timer(20, units="us")
+            dut.mouse_clk_in = 0
+            await Timer(20, units="us")
+        await Timer(20, units="us")
+
+        dut.mouse_clk_in = 1
+        dut.mouse_data_in = 1
+        await Timer(200, units="us")
+    
+async def mouse_tester(dut):
+    cocotb.fork(mouse(dut))
+    def send(code): codes_to_send.append(code)
+    def move_mouse(xrel, yrel):
+        yrel = -yrel;
+        send(0b00001000 |
+                   ((xrel >> 31) << 4) |
+                   ((yrel >> 31) << 5));
+        send(xrel & 0xFF)
+        send(yrel & 0xFF)
+
+    await Timer(50, units="us")
+    move_mouse(5, 10)
+
 H_RES = 640
 V_RES = 480
 
@@ -43,6 +101,8 @@ async def test_wiggly_ic_1(dut):
 
     await Timer(500, units="ns")  # wait a bit
 
+    cocotb.fork(mouse_tester(dut))
+
     screenbuffer = [0]*(H_RES*V_RES*3)
     def index(x, y): return (y*H_RES + x) * 3
 
@@ -66,7 +126,8 @@ async def test_wiggly_ic_1(dut):
 
         assert screenbuffer[index(0, 0)] == 0b11 << 6
 
-         # blue cursor
+        # blue cursor
+        info("mouse_x=%d, mouse_y=%d", dut.mouse_x.value, dut.mouse_y.value)
         cursor = index(dut.mouse_x.value, dut.mouse_y.value)
         assert screenbuffer[cursor] == 0b00 << 6
         assert screenbuffer[cursor + 1] == 0b00 << 6
